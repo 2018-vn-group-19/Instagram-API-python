04a629f1b8d6dc53e7a81bc9dcb637a0df46e6c0
Grupo19 <40037939+2018-vn-group-19@users.noreply.github.com>
2019-03-03 19:17:11 -0300

    Update upload_photo.py

--- examples/upload_photo.py
+++ examples/upload_photo.py
@@ -1,10 +1,48 @@
 # coding: utf-8
 import time
+from PIL import Image
 from InstagramAPI import InstagramAPI
 
 #ACA EN VEZ DE jnqov4 VA TU USARIO Y TU CONTRASEÑA
 InstagramAPI = InstagramAPI("dankentina", "quickstart")
 InstagramAPI.login()  # login
+
+
+
+basewidth = 300
+img = Image.open('dank/0001.jpg')
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+img.save('dank/0001.jpg') 
+
+basewidth = 300
+img = Image.open('dank/0002.jpg')
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+img.save('dank/0002.jpg') 
+
+basewidth = 300
+img = Image.open('dank/0003.jpg')
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+img.save('dank/0003.jpg') 
+
+basewidth = 300
+img = Image.open('dank/0004.jpg')
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+img.save('dank/0004.jpg') 
+
+basewidth = 300
+img = Image.open('dank/0005.jpg')
+wpercent = (basewidth/float(img.size[0]))
+hsize = int((float(img.size[1])*float(wpercent)))
+img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+img.save('dank/0005.jpg') 
 
 photo_path = 'dank/0001.jpg'
 caption = "#dank #dankmeme #dankest #dankmemesargentina #dankentina"
